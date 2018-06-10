159756884bad6641b16c4bdece1dbfe9f6fd0103
Roman Kotov <waydeveloper@gmail.com>
2018-06-11 00:35:17 +0300

    Initial Commit

---
+++ RKFoundationExtensions/RKFoundationExtensions.h
@@ -0,0 +1,19 @@
+//
+//  RKFoundationExtensions.h
+//  RKFoundationExtensions
+//
+//  Created by Roman Kotov on 11/06/2018.
+//  Copyright © 2018 Roman Kotov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKFoundationExtensions.
+FOUNDATION_EXPORT double RKFoundationExtensionsVersionNumber;
+
+//! Project version string for RKFoundationExtensions.
+FOUNDATION_EXPORT const unsigned char RKFoundationExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RKFoundationExtensions/PublicHeader.h>
+
+
